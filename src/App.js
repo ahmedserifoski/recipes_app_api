c67fe2a40fcaf07a28dddcfd1a72fe6365254ac9
Ahmed Serifoski <ahmed.serifoski@gmail.com>
2020-09-21 12:18:56 +0200

    change get function

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, {Component} from 'react';
 import './App.css';
 import Form from "./Components/Form"
 import Recipes from "./Components/Recipes"
+
     const API_ID = "22d57a35"
     const API_KEY = "1521a8a638ba395189e881d0162d51d6"
     
@@ -23,17 +24,29 @@ class App extends Component {
     console.log(this.state.recipes)
   }
 
-  componentDidMount = () => {
-    const json = localStorage.getItem("recipes")
-    const recipes = JSON.parse(json)
-    this.setState({ recipes })
+  // getRandomRecipe = async() => {
+    
+  //   const example_link = `https://cors-anywhere.herokuapp.com/https://api.edamam.com/search?app_id=${API_ID}&app_key=${API_KEY}&q=burger
+  //   `
+  //   const res = await fetch(example_link)
+  //   const data = await res.json()
+  //   this.setState( {recipes: data.hits} )
+  //   console.log(this.state.recipes)
+  // }
 
-  }
+  // componentDidMount = () => {
+  //   this.getRandomRecipe()
+  //   const json = localStorage.getItem("recipes")
+  //   const recipes = JSON.parse(json)
+  //   this.setState({ recipes })
+    
+    
+  // }
 
-  componentDidUpdate = () => {
-    const recipes = JSON.stringify(this.state.recipes)
-    localStorage.setItem("recipes", recipes)
-  }
+  // componentDidUpdate = () => {
+  //   const recipes = JSON.stringify(this.state.recipes)
+  //   localStorage.setItem("recipes", recipes)
+  // }
 
   render() {
     return(
