9f5b2258d8e306c3d9207f4b8d6d073cffc521ea
Ahmed Serifoski <ahmed.serifoski@gmail.com>
2020-09-22 11:09:09 +0200

    add food favicon

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -7,7 +7,7 @@ const Form = ( {getRecipe }) => {
                 <input
                     type="text"
                     name="recipeName"
-                    placeholder="Recipe"
+                    placeholder="Recipenpm"
                     className="form__input"
                  />
                 <button className="form__button">Search</button>
